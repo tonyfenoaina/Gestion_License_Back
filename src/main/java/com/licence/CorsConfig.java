859d3d45987d2e1330d955976768a5dd69077beb
Finaritra Andriantahiana <fyandriantahiana@gmail.com>
2024-09-14 01:50:56 +0300

    Correction erreur CORS

---
+++ src/main/java/com/licence/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.licence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // Autoriser les informations d'identification (cookies)
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Utilise la propriété de configuration
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Appliquer à toutes les routes
+
+        return new CorsFilter(source);
+    }
+}
+
--- src/main/java/com/licence/controllers/AuthController.java
+++ src/main/java/com/licence/controllers/AuthController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController{
